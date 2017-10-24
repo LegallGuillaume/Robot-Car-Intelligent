934a44cfdf5f27059ffdcd6d32d7d5a085435894
glegall <glegall@localhost>
2017-10-24 14:05:55 +0200

    update definition.h with server

--- headers/definition.h
+++ headers/definition.h
@@ -1,3 +1,5 @@
+/*MATRICE SCENE DEFINITION*/
+
 #define VIDE_DEF                0
 #define CAR_DEF                 1
 #define BLOC_DEF                -2
@@ -5,10 +7,14 @@
 #define ARRIVE_DEF              4
 #define CHEMIN_FAIT_DEF         5
 
+/*SCENE DEFINITION*/
 
 #define CAM_WIDTH               640
 #define CAM_HEIGHT              480
-
-
 #define MAX_BLOC_SCENE          30
 #define ECHANTILLION_CALIB      40
+
+/*SERVEUR DEFINITION*/
+
+#define MAX_CONNECTION_CLIENT   10
+#define PORT_SERVEUR            975
